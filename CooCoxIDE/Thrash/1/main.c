2b7402534668a74bb75dc5c06b5b62c2523a55db
Shneider <Shneider@users.noreply.github.com>
2013-08-23 17:18:07 +0300

    code rev 1 ololololololo

--- CooCoxIDE/Thrash/1/main.c
+++ CooCoxIDE/Thrash/1/main.c
@@ -1,77 +1,65 @@
-
-
-
+// written by Shneider.
 #include<stm32f10x_rcc.h>
 #include<stm32f10x_gpio.h>
-
 #include "stm32f10x.h"
 #include "stm32f10x_conf.h"
 
-void Delay(volatile uint32_t nCount);
-
-
-GPIO_InitTypeDef GPIO_InitStructure;
-
-volatile int main(void)
+void Delay(volatile uint32_t nCount)
 {
+    for (; nCount > 0; nCount--);
+}
 
+
+
+
+
+//========================================================= Main =================================================================================
+int main(void)
+{
 	unsigned char count;
-// init for GPIO (LED)
+        // init for GPIO (LED)
+        GPIO_InitTypeDef GPIO_InitStructure;
         RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
         GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
         GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_PP;
         GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5;       // two LED (guess on what pin!!)
         GPIO_Init(GPIOC, &GPIO_InitStructure);
 
-      GPIOC->ODR |= (1<<4|1<<3|1<<5);
-      Delay(8000000);
-      GPIOC->ODR &= (0<<4|0<<3|0<<5);
-count = 0;
-    while(1)
-
-    {
-
-    	while (count<10)
-    	{
-    	Delay(8000000);
-    	   GPIOC->ODR &= (0<<4|0<<3|0<<5);
-    	      Delay(8000000);
-    	      GPIOC->ODR |= (1<<4|1<<3|1<<5);
-    	      count++;
+        GPIOC->ODR |= (1<<4|1<<3|1<<5);
+        Delay(8000000);
+        GPIOC->ODR &= (0<<4|0<<3|0<<5);
+        count = 0;
+         while (count<10)
+         {
+    	    Delay(8000000);
+    	    GPIOC->ODR &= (0<<4|0<<3|0<<5);
+    	    Delay(8000000);
+    	    GPIOC->ODR |= (1<<4|1<<3|1<<5);
+    	    count++;
     	}
-
+    	
     	Delay(4000000);
-
-
+while(1)
+{
 
         GPIO_WriteBit(GPIOC,GPIO_Pin_3,Bit_RESET);
         GPIO_WriteBit(GPIOC,GPIO_Pin_5,Bit_RESET);
-                GPIO_WriteBit(GPIOC,GPIO_Pin_4,Bit_SET);
-                Delay(8000000);
-                GPIO_WriteBit(GPIOC,GPIO_Pin_4,Bit_RESET);
-                GPIO_WriteBit(GPIOC,GPIO_Pin_3,Bit_SET);
-                GPIO_WriteBit(GPIOC,GPIO_Pin_5,Bit_RESET);
-                Delay(8000000);
-                GPIO_WriteBit(GPIOC,GPIO_Pin_4,Bit_RESET);
-                               GPIO_WriteBit(GPIOC,GPIO_Pin_3,Bit_RESET);
-                               GPIO_WriteBit(GPIOC,GPIO_Pin_5,Bit_SET);
-                               Delay(8000000);
-
-
-
-
-
+        GPIO_WriteBit(GPIOC,GPIO_Pin_4,Bit_SET);
+        Delay(8000000);
+        GPIO_WriteBit(GPIOC,GPIO_Pin_4,Bit_RESET);
+        GPIO_WriteBit(GPIOC,GPIO_Pin_3,Bit_SET);
+        GPIO_WriteBit(GPIOC,GPIO_Pin_5,Bit_RESET);
+        Delay(8000000);
+        GPIO_WriteBit(GPIOC,GPIO_Pin_4,Bit_RESET);
+        GPIO_WriteBit(GPIOC,GPIO_Pin_3,Bit_RESET);
+        GPIO_WriteBit(GPIOC,GPIO_Pin_5,Bit_SET);
+        Delay(8000000);
 
     }
 
 }
 
-
-//-------
-void Delay(volatile uint32_t nCount)
-{
-    for (; nCount > 0; nCount--);
-}
+//============================================================ end =======================================================================
 
 
 
